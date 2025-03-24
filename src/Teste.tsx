99803385c23b2efca0e8f31882c2c73103914b4c
Nayara <nayaramaisa.stn@gmail.com>
2025-03-24 07:04:56 -0300

    configurando stylled components

---
+++ src/Teste.tsx
@@ -0,0 +1,15 @@
+
+import styled from 'styled-components';
+
+const Botao = styled.button`
+  background-color:red;
+`
+
+
+function Teste() {
+return(
+  <Botao>Clique aqui</Botao>
+)
+}
+
+export default Teste;
